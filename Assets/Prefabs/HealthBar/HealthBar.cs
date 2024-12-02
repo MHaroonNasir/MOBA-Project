de52a6718bb421b8e5d1c352b49da0905f0d99af
OMA <46166498+MHaroonNasir@users.noreply.github.com>
2024-12-02 20:39:33 +0000

    Healthbar draft 3

--- Assets/Prefabs/HealthBar/HealthBar.cs
+++ Assets/Prefabs/HealthBar/HealthBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,15 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public float currentHealth, maxHealth, incrementHealth;
+    public Slider healthBarSlider;
+
+    public List<GameObject> indicators; //these 2 lists should be same size
+    public List<Image> indicatorImages;
 
     void Start()
     {
-        
+        UpdateHealthBarSlider();
+        UpdateIndicators();
     }
 
     public void UpdateCurrentHealth(float currentHealth) {
@@ -17,6 +23,29 @@ public class HealthBar : MonoBehaviour
     }
 
     public void UpdateMaxHealth(float maxHealth) {
-        this.maxHealth = maxHealth;
+        this.maxHealth = Mathf.Min(maxHealth, 3000f);
+    }
+
+    private void UpdateHealthBarSlider() {
+        healthBarSlider.maxValue = maxHealth;
+        healthBarSlider.value = currentHealth;
+    }
+
+    private void UpdateIndicators() {
+        //casting to int
+        int numOfIndicators = (int)Mathf.Floor(maxHealth / currentHealth); //mathf.floor to ensure 799 / 100 leads to 7 indicator images, not rounded up 8
+
+        for (int i = 0; i < indicators.Count; i++) {
+            if (i < numOfIndicators) {
+                indicators[i].SetActive(true);
+                if (i == numOfIndicators - 1) {
+                    indicatorImages[i].enabled = false;
+                } else {
+                    indicatorImages[i].enabled = true;
+                }
+            } else {
+                indicators[i].SetActive(false);
+            }
+        }
     }
 }

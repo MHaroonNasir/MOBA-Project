102b32d50501e92863b32af07695b2839a2dd3a8
OMA <46166498+MHaroonNasir@users.noreply.github.com>
2024-12-17 18:07:43 +0000

    Restructured slow cc scripts

--- Assets/Scripts/Crowd Control/CCSlow.cs
+++ Assets/Scripts/Crowd Control/CCSlow.cs
@@ -5,10 +5,30 @@ using UnityEngine;
 public class CCSlow : CombatSystem
 {
     public void ReceiveCCSlow(float duration, char slowType, float intensity) {
-        StartCoroutine(ApplyCCSlow(duration, slowType, intensity));
+        //StartCoroutine(ApplyCCSlow(duration, slowType, intensity));
+        ApplyCCSlow(duration, slowType, intensity);
     }
 
-    private IEnumerator ApplyCCSlow(float duration, char slowType, float intensity) {
+    public void ReceiveCCSlow(char slowType, float intensity) {
+        //StartCoroutine(ApplyCCSlow(duration, slowType, intensity));
+        ApplyCCSlow(slowType, intensity);
+    }
+
+    void ApplyCCSlow(float duration, char slowType, float intensity) {
+    }
+
+    void ApplyCCSlow(char slowType, float intensity) {
+        if (slowType == 'f') {
+            stats.ID.appliedMovementSpeed = stats.ID.baseMovementSpeed - intensity;
+        }
+
+        if (slowType == '%') {
+            stats.ID.appliedMovementSpeed = stats.ID.baseMovementSpeed * (1 - intensity);
+        }
+        stats.ID.isSlowed?.Invoke();
+    }
+
+    /*private IEnumerator ApplyCCSlow(float duration, char slowType, float intensity) {
         float timePassed = 0f;
 
         while (timePassed < duration && slowType == 'f') {
@@ -29,5 +49,5 @@ public class CCSlow : CombatSystem
         stats.ID.appliedMovementSpeed = stats.ID.baseMovementSpeed;
         Debug.Log("out of while loop: " + stats.ID.appliedMovementSpeed);
         stats.ID.isSlowed?.Invoke();
-    }
+    }*/
 }
--- Assets/Scripts/Crowd Control/SlowHitbox.cs
+++ Assets/Scripts/Crowd Control/SlowHitbox.cs
@@ -4,55 +4,59 @@ using UnityEngine;
 
 public class SlowHitbox : MonoBehaviour
 {
-    //Dictionary<string, CCSlow> chrIDs = new Dictionary<string, CCSlow>();
-    List<string> chrNames = new List<string>();
-    List<Stats> chrStats = new List<Stats>();
-    List<CCSlow> chrSlows = new List<CCSlow>();
+    Dictionary<string, CCSlow> chrIDs = new Dictionary<string, CCSlow>();
+    //List<string> chrNames = new List<string>();
+    //List<CCSlow> chrSlows = new List<CCSlow>();
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
             other.gameObject.TryGetComponent<Stats>(out Stats stats);
             other.gameObject.TryGetComponent<CCSlow>(out CCSlow ccSlow);
-            //chrIDs.Add(stats.ID.playerName, ccSlow);
-            if (chrNames.Contains(stats.ID.playerName) == false) {
+            chrIDs.Add(stats.ID.playerName, ccSlow);
+            ApplySlowToEnemy(stats.ID.playerName);
+            /*if (chrNames.Contains(stats.ID.playerName) == false) {
                 chrNames.Add(stats.ID.playerName);
-                chrStats.Add(stats);
                 chrSlows.Add(ccSlow);
-            }
-            StartCoroutine(ApplyContinuousSlow(stats.ID.playerName));
+            }*/
+            //StartCoroutine(ApplyContinuousSlow(stats.ID.playerName));
         }
     }
 
+    void ApplySlowToEnemy(string playerName) {
+        chrIDs[playerName].ReceiveCCSlow('%', 0.5f);
+    }
+
     IEnumerator ApplyContinuousSlow(string playerName) {
-        /*while (chrIDs.ContainsKey(playerName) == true) {
-            chrIDs[playerName].ReceiveCCSlow(0.2f, '%', 0.65f);
+        while (chrIDs.ContainsKey(playerName) == true) {
+            chrIDs[playerName].ReceiveCCSlow(0.16f, '%', 0.65f);
             
-            yield return new WaitForSeconds(0.2f);
-        }*/
-        while (chrNames.Contains(playerName) == true) {
+            yield return new WaitForSeconds(0.15f);
+        }
+        /*while (chrNames.Contains(playerName) == true) {
             int index = chrNames.IndexOf(playerName);
-            //chrStats[index].ID.isSlowed?.Invoke();
             chrSlows[index].ReceiveCCSlow(0.16f, '%', 0.65f);
             Debug.Log("recieve cc slow");
             yield return new WaitForSeconds(0.15f);
-        }
+        }*/
 
     }
 
     private void OnTriggerExit(Collider other) {
         other.gameObject.TryGetComponent<Stats>(out Stats stats);
-        //chrIDs.Remove(stats.ID.playerName);
-        int index = chrNames.IndexOf(stats.ID.playerName);
-        chrNames.RemoveAt(index);
-        chrStats.RemoveAt(index);
-        chrSlows.RemoveAt(index);
-        Debug.Log("removed from cc slow");
+        chrIDs[stats.ID.playerName].ReceiveCCSlow('%', 0f);
+        chrIDs.Remove(stats.ID.playerName);
+        //int index = chrNames.IndexOf(stats.ID.playerName);
+        //chrNames.RemoveAt(index);
+        //chrSlows.RemoveAt(index);
+        //Debug.Log("removed from cc slow");
     }
 
     private void OnDisable() {
-        //chrIDs.Clear();
-        chrNames.Clear();
-        chrStats.Clear();
-        chrSlows.Clear();
+        foreach (KeyValuePair<string, CCSlow> entry in chrIDs) {
+            entry.Value.ReceiveCCSlow('%', 0f);
+        }
+        chrIDs.Clear();
+        //chrNames.Clear();
+        //chrSlows.Clear();
     }
 }

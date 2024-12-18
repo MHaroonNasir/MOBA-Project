ee07808ca8bb8829cd3e996f86657a7c7fbe244e
OMA <46166498+MHaroonNasir@users.noreply.github.com>
2024-12-18 19:53:55 +0000

    Added duration slow code

--- Assets/Scripts/Crowd Control/CCSlow.cs
+++ Assets/Scripts/Crowd Control/CCSlow.cs
@@ -14,7 +14,23 @@ public class CCSlow : CombatSystem
         ApplyCCSlow(slowType, intensity);
     }
 
-    void ApplyCCSlow(float duration, char slowType, float intensity) {
+    IEnumerator ApplyCCSlow(float duration, char slowType, float intensity) {
+        float timePassed = 0f;
+
+        if (slowType == 'f') {
+            stats.ID.appliedMovementSpeed = stats.ID.baseMovementSpeed - intensity;
+        }
+        if (slowType == '%') {
+            stats.ID.appliedMovementSpeed = stats.ID.baseMovementSpeed * (1 - intensity);
+        }
+        stats.ID.isSlowed?.Invoke();
+
+        while (timePassed < duration) {
+                timePassed += Time.deltaTime;
+                yield return null; //return after each frame
+            }
+        stats.ID.appliedMovementSpeed = stats.ID.baseMovementSpeed;
+        stats.ID.isSlowed?.Invoke();
     }
 
     void ApplyCCSlow(char slowType, float intensity) {

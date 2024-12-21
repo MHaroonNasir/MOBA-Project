8405711aac8ac666f6f1e5cae1b8a64bedeecd2c
OMA <46166498+MHaroonNasir@users.noreply.github.com>
2024-12-21 14:45:26 +0000

    update player stats

--- Assets/PlayerID.cs
+++ Assets/PlayerID.cs
@@ -8,14 +8,44 @@ using UnityEngine.AI;
 public class PlayerID : ScriptableObject
 {
     public string playerName = "samurai";
-    public float baseMovementSpeed = 3.5f;
-    public float appliedMovementSpeed = 3.5f;
+
+    //health and sheilds
+    public float baseHealth = 100f;
+    public float appliedHealth = 100f;
+    public float baseHealthRegen = 1.667f;
+    public float appliedHealthRegen = 1.667f;
+    public float shieldAmount = 0f;
+
+    //mana
+    public float baseMana = 100f;
+    public float appliedMana = 100f;
+    public float baseManaRegen = 5f;
+    public float appliedManaRegen = 5f;
+
+    //ressistances
+    public float basePhysicalDefense = 0f;
+    public float appliedPhysicalDefense = 0f;
+    public float baseMagicDefense = 0f;
+    public float appliedMagicDefense = 0f;
+
+    //damage
+    public float basePhysicalDamage = 10f;
+    public float appliedPhysicalDamage = 10f;
+    public float baseMagicDamage = 10f;
+    public float appliedMagicDamage = 10f;
+
+    //attack speed and range
     public float baseAttackSpeed = 1f;
     public float appliedAttackSpeed = 1f;
-    public float attackRange = 3f;
+    public float baseAttackRange = 3f;
+    public float appliedAttackRange = 3f;
 
-    public float health = 100f;
-    public float attackDamage = 10f;
+    //critical strike
+    //public int critStrikeChance;
+
+    //movement speed
+    public float baseMovementSpeed = 3.5f;
+    public float appliedMovementSpeed = 3.5f;
 
     //public Action testAction;
     public PlayerEvents events; //struct //allocated on stack, not heap (research this)

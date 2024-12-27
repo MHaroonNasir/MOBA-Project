2d994aee06e408689f057ec9f6436189ba73d9b6
OMA <46166498+MHaroonNasir@users.noreply.github.com>
2024-12-27 20:43:55 +0000

    setup for script layout

---
+++ Assets/Scripts/Player Info/CharacterInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInfo : MonoBehaviour
+{
+    public GenericCharacterStatsAndActions genericStatsAndActions;
+
+    public Action isNotInCombat;
+}

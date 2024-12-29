d56586dc8a03ce34574079949d05c7b334fcdc64
OMA <46166498+MHaroonNasir@users.noreply.github.com>
2024-12-29 20:01:25 +0000

    update script name and location

---
+++ Assets/Scripts/Player Info/SamuraiCharacterTemplate.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SamuraiCharacterTemplate : MonoBehaviour
+{
+    protected SamuraiCharacterInfo samuraiCharacterInfo;
+    protected PlayerMove playerMove;
+    //protected EnemyInteraction enemyInteraction;
+
+    protected void Awake() {
+        samuraiCharacterInfo = transform.root.GetComponent<SamuraiCharacterInfo>();
+        playerMove = transform.root.GetComponent<PlayerMove>();
+        //enemyInteraction = transform.root.gameObject.GetComponent<EnemyInteraction>();
+    }
+    
+}
